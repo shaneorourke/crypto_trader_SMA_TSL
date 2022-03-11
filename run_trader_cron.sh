6cacbf861df5f9a888e0ec2edb04ef8a472879ee
shaneorourke <shaneorourke110@gmail.com>
2022-03-11 15:30:35 +0000

    Add files via upload

--- run_trader_cron.sh
+++ run_trader_cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . /home/pi/.bashrc
 
+cd /home/pi/Documents/git/crypto_trader_SMA_TSL &&
 source /home/pi/Documents/git/crypto_trader_SMA_TSL/trader/bin/activate &&
 python /home/pi/Documents/git/crypto_trader_SMA_TSL/trader_no_console_cron.py
