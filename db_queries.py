fbf369ac12f96568258c26f1f2f5ad513073acb4
shaneorourke <shaneorourke110@gmail.com>
2022-03-14 22:13:36 +0000

    Add files via upload

--- db_queries.py
+++ db_queries.py
@@ -46,11 +46,16 @@ for curr in currencies:
     buy_price = c.fetchall()
     buy_price = clean_up_sql_out(buy_price,1)
 
+    ## Made First Sale
+    c.execute(f'SELECT count(*) FROM orders WHERE Currency="{curr}" and market = "SELL"')
+    sale_made = c.fetchall()
+    sale_made = clean_up_sql_out(sale_made,1)
+
     ## Position Open
     c.execute(f'SELECT position FROM position WHERE Currency="{curr}"')
     result = c.fetchall()
     pos = clean_up_sql_out(result,0)
-    if pos == '0':
+    if pos == 0:
         position = 'BUYING'
     else:
         position = 'SELLING'
@@ -68,18 +73,27 @@ for curr in currencies:
                 select * from order_check""")
     result = c.fetchall()
     curr_profit = clean_up_sql_out(result,1)
-    profit = round((float(curr_profit)/float(price['price']))*100,2)
-    print(f'Profit Percentage:{profit}%')
+    if sale_made !='0':
+        if curr_profit != 'None':
+            profit = round((float(curr_profit)/float(price['price']))*100,2)
+            print(f'Profit Percentage:{profit}%')
+            qty = 0.001
+            usdt_value = float(price['price']) * qty
+            usdt_profit = usdt_value*(profit/100)
+            print(f'USDT Profit:${round(usdt_profit,2)}')
 
-    ## Stop Details
-    c.execute('SELECT round(stop_price,2) FROM trailing_stop_loss ORDER BY market_date DESC LIMIT 1')
-    result = c.fetchall()
-    print(f'Last Stop Price:{clean_up_sql_out(result,1)}')
 
-    ## Original Stop Details Est
-    c.execute('SELECT round(price-(price * 0.03),2) FROM orders WHERE market = "BUY" ORDER BY market_date DESC LIMIT 1')
+
+
+    ## Take Profit Details Est
+    c.execute('SELECT round(price+(price * 0.01),2) FROM orders WHERE market = "BUY" ORDER BY market_date DESC LIMIT 1')
     result = c.fetchall()
-    print(f'Original Stop Est:{clean_up_sql_out(result,1)}')
+    print(f'Take Profit:{clean_up_sql_out(result,1)}')
+
+    ## Stop Details Est
+    c.execute('SELECT round(price-(price * 0.015),2) FROM orders WHERE market = "BUY" ORDER BY market_date DESC LIMIT 1')
+    result = c.fetchall()
+    print(f'Stop Limit:{clean_up_sql_out(result,1)}')
 
 
     ## Orders
@@ -96,5 +110,11 @@ c.execute(f"""with last_order as (select market, market_date from orders ORDER B
             select * from order_check""")
 result = c.fetchall()
 tot_profit = clean_up_sql_out(result,1)
-total_profit = round((float(curr_profit)/float(price['price']))*100,2)
-print(f'##### Total Profit Percentage:{total_profit}%')
+if sale_made !='0':
+    if tot_profit != 'None':
+        total_profit = round((float(curr_profit)/float(price['price']))*100,2)
+        print(f'##### Total Profit Percentage:{total_profit}%')
+        qty = 0.001
+        usdt_value = float(price['price']) * qty
+        usdt_profit = usdt_value*(total_profit/100)
+        print(f'Total USDT Profit:${round(usdt_profit,2)}')
